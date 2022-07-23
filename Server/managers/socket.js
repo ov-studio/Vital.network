10c5e9c01622431388fc987ad419f6282e1c398b
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-23 18:57:44 +0300

    Update socket.js

--- Server/managers/socket.js
+++ Server/managers/socket.js
@@ -13,6 +13,8 @@
 -- Imports --
 -----------*/
 
+const CWS = require("ws")
+const CUtility = require("../utilities")
 const CServer = require("./server")(true)
 
 
@@ -20,15 +22,62 @@ const CServer = require("./server")(true)
 -- Class: CSocket --
 ------------------*/
 
-CServer.socket = {
-    config: {},
-    instance: {},
-    room: {}
-}
+class CSocket {
+    /////////////////////
+    // Static Mmebers //
+    ////////////////////
 
-CServer.socket.connect = function() {
-    CServer.instance.CWS.on("connection", function(socket, request) {
-        CServer.socket.instance[socket] = {}
-        console.log("CONNECTED SOCKET")
-    })
+    static route = {}
+
+    static isVoid(route) {
+        return (CUtility.isString(route) && !CUtility.isObject(CServer.socket.route[route]) && true) || false
+    }
+
+    static create(route) {
+        if (!CServer.socket.isVoid(route)) return false
+        CServer.socket.route[route] = new CServer.socket(route)
+        return true
+    }
+
+    static destroy(route) {
+        if (CServer.socket.isVoid(route)) return false
+        CServer.socket.route[route].isUnloaded = true
+        delete CServer.socket.route[route]
+        return true
+    }
+
+
+    ///////////////////////
+    // Instance Mmebers //
+    //////////////////////
+
+    constructor(route) {
+        const self = this
+        self.route = route
+        self.instance = {}, self.room = {}
+        self.server = new CWS.Server({
+            noServer: true,
+            path: "/" + self.route
+        })
+        self.server.on("connection", function(socket, request) {
+            self.instance[socket] = {}
+            // TODO: REMOVE LATER
+            CUtility.print("CONNECTED SOCKET")
+        })
+    }
+
+    isValid() {
+        const self = this
+        return (!self.isUnloaded && self.route && CSocket.route[(self.route)] && true) || false
+    }
+
+    destroy() {
+        const self = this
+        if (!self.isValid()) return false
+        CServer.socket.destroy(this.route)
+        return true
+    }
 }
+CServer.socket = CSocket
+
+const testRT = CServer.socket.create("test")
