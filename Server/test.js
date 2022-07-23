8871780b7114604e9f14cbc8f2ccd9f6d6e01759
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-23 23:36:05 +0300

    Update test.js

--- Server/test.js
+++ Server/test.js
@@ -6,30 +6,31 @@
      Developer(s): Aviril, Mario, Tron
      DOC: 22/07/2022
      Desc: Module Tester
-----------------------------------------------------------------*//*-----------
+----------------------------------------------------------------*/
+
+
+/*-----------
 -- Imports --
 -----------*/
 
-// TODO: FIX, INDEX & PARSE ALL DEEP FUNCTIONS
-CServer = require("./managers/server")()
-
+const CNetworkify = require("./managers/server")()
+console.log(CNetworkify)
 
 /*--------
 -- Test --
 --------*/
 
 async function test() {
-    const isConnected = await(CServer.connect(33001, {
+    const isConnected = await(CNetworkify.connect(33001, {
         isCaseSensitive: true
     }))
     if (!isConnected) return false
+    CNetworkify.socket.create("test")
 
     // @Rest API Example
-    /*
-    CServer.createRestAPI("get", "", function(request, response) {
+    CNetworkify.createRestAPI("get", "", function(request, response) {
         response.status(200).send("Some status message")
     })
-    CServer.destroyRestAPI("get", "")
-    */
+    CNetworkify.destroyRestAPI("get", "")
 }
 test()
