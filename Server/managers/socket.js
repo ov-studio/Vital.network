c5f13d645a4eb6302ccfef4412b96ef9b2fbf4ba
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-24 18:31:46 +0300

    Added client side socket resolver

--- Server/managers/socket.js
+++ Server/managers/socket.js
@@ -108,27 +108,15 @@ CServer.socket.addInstanceMethod("off", function(self, name, ...cArgs) {
 if (!CUtility.isServer) {
     CServer.socket.addMethod("constructor", function(self, route) {
         CUtility.fetchVID(self)
-        self.route = route, self.network = {}
-        self.room = {}
+        self.route = route, self.network = {}, self.room = {}
+        self.isConnected = false
+        var cResolver = false
+        CServer.config.isAwaiting = new Promise((resolver) => cResolver = resolver)
         self.server = new WebSocket(`${CServer.config.protocol}//${CServer.config.hostname}:${port}/${route}`)
-        /*
-        self.server.on("onClientConnect", function(socket, request) {
-            var [instance, query] = request.url.split("?")
-            instance = CServer.socket.fetch(instance.slice(1))
-            if (!instance) return false
-            const vid = CUtility.fetchVID(socket)
-            self.instance[vid] = socket
-            query = CUtility.queryString.parse(query)
-            socket.on("close", function() {
-                delete self.instance[(socket.vid)]
-            })
-            socket.on("message", function(payload) {
-                payload = JSON.parse(payload)
-                if (!CUtility.isObject(payload) || !CUtility.isString(payload.networkName) || !CUtility.isArray(payload.networkArgs)) return false
-                self.emit(payload.networkName, null, ...payload.networkArgs)
-            })
+        self.server.on("open", function() {
+            self.isConnected = true
+            cResolver(self.isConnected)
         })
-        */
     })
 }
 else {
