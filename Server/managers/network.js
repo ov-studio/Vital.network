db02a0050a52e5b950c09c5040743696b31f9604
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-24 17:18:53 +0300

    Update network.js

--- Server/managers/network.js
+++ Server/managers/network.js
@@ -17,129 +17,121 @@ const CUtility = require("../utilities")
 const CServer = require("./server")
 
 
-/*-----------------
--- Class: Socket --
------------------*/
+/////////////////////
+// Class: Network //
+/////////////////////
 
-class CNetwork {
-    /////////////////////
-    // Static Mmebers //
-    ////////////////////
-
-    static isClass = true
-    static buffer = {}
-
-    static isVoid = function(name) {
-        return (CUtility.isString(name) && !CUtility.isObject(CServer.network.buffer[name]) && true) || false
-    }
-
-    static fetch = function(name) {
-        return (!CServer.network.isVoid(name) && CServer.network.buffer[name]) || false
-    }
-
-    static create = function(name, ...cArgs) {
-        if (!CServer.isConnected(true) || !CServer.network.isVoid(name)) return false
-        CServer.network.buffer[name] = new CServer.network(name, ...cArgs)
-        return CServer.network.buffer[name]
-    }
-
-    static destroy = function(name) {
-        if (CServer.network.isVoid(name)) return false
-        CServer.network.buffer[name].isUnloaded = true
-        delete CServer.network.buffer[name]
-        return true
-    }
-
-    static on = function(name, ...cArgs) {
-        const cInstance = CServer.network.fetch(name)
-        if (!cInstance) return false
-        return cInstance.on(...cArgs)
-    }
-
-    static off = function(name, ...cArgs) {
-        const cInstance = CServer.network.fetch(name)
-        if (!cInstance) return false
-        return cInstance.off(...cArgs)
-    }
-
-    static emit = function(name, ...cArgs) {
-        const cInstance = CServer.network.fetch(name)
-        if (!cInstance) return false
-        return cInstance.emit(...cArgs)
-    }
+CServer.network = CUtility.createClass({
+    buffer: {}
+})
 
 
-    ///////////////////////
-    // Instance Mmebers //
-    //////////////////////
+/////////////////////
+// Static Members //
+/////////////////////
 
-    constructor(name, isCallback) {
-        const self = this
-        self.name = name
-        self.isCallback = (CUtility.isBool(isCallback) && true) || false
-        self.handler = (!self.isCallback && {}) || false
-    }
-
-    isInstance() {
-        const self = this
-        return (!self.isUnloaded && !CServer.network.isVoid(self.name) && true) || false
-    }
-
-    destroy() {
-        const self = this
-        if (!self.isInstance()) return false
-        CServer.network.destroy(self.name)
-        return true
-    }
-
-    on(exec) {
-        const self = this
-        if (!self.isInstance() || !CUtility.isFunction(exec)) return false
-        const vid = CUtility.fetchVID(exec)
-        if (!self.isCallback) {
-            if (self.handler[vid]) return false
-            self.handler[vid] = {
-                exec: exec
-            }
-        }
-        else {
-            if (self.handler) return false
-            self.handler = {
-                exec: exec
-            }
-        }
-        return true
-    }
-
-    off(exec) {
-        const self = this
-        if (!self.isInstance() || !CUtility.isFunction(exec)) return false
-        if (!self.isCallback) {
-            const vid = CUtility.fetchVID(exec)
-            if (!self.handler[vid]) return false
-            delete self.handler[vid]
-        }
-        else {
-            if (!self.handler || (CUtility.fetchVID(exec) != CUtility.fetchVID(self.handler))) return false
-            self.handler = false
-        }
-        return true
-    }
-
-    emit(...cArgs) {
-        const self = this
-        if (!self.isInstance() || self.isCallback) return false
-        for (const i in self.handler) {
-            const j = self.handler[i]
-            j.exec(...cArgs)
-        }
-        return true
-    }
-
-    async emitCallback(...cArgs) {
-        const self = this
-        if (!self.isInstance() || !self.isCallback || !self.handler) return false
-        return await self.handler.exec(...cArgs)
-    }
+CServer.network.addMethod("isVoid", function(name) {
+    return (CUtility.isString(name) && !CUtility.isObject(CServer.network.buffer[name]) && true) || false
 }
-CServer.network = CNetwork
+
+CServer.network.addMethod("fetch", function(name) {
+    return (!CServer.network.isVoid(name) && CServer.network.buffer[name]) || false
+}
+
+CServer.network.addMethod("create", function(name, ...cArgs) {
+    if (!CServer.isConnected(true) || !CServer.network.isVoid(name)) return false
+    CServer.network.buffer[name] = new CServer.network(name, ...cArgs)
+    return CServer.network.buffer[name]
+}
+
+CServer.network.addMethod("destroy", function(name) {
+    if (CServer.network.isVoid(name)) return false
+    CServer.network.buffer[name].isUnloaded = true
+    delete CServer.network.buffer[name]
+    return true
+}
+
+CServer.network.addMethod("on", function(name, ...cArgs) {
+    const cInstance = CServer.network.fetch(name)
+    if (!cInstance) return false
+    return cInstance.on(...cArgs)
+}
+
+CServer.network.addMethod("off", function(name, ...cArgs) {
+    const cInstance = CServer.network.fetch(name)
+    if (!cInstance) return false
+    return cInstance.off(...cArgs)
+}
+
+CServer.network.addMethod("emit", function(name, ...cArgs) {
+    const cInstance = CServer.network.fetch(name)
+    if (!cInstance) return false
+    return cInstance.emit(...cArgs)
+}
+
+
+///////////////////////
+// Instance Mmebers //
+//////////////////////
+
+isInstance() {
+    const self = this
+    return (!self.isUnloaded && !CServer.network.isVoid(self.name) && true) || false
+}
+
+destroy() {
+    const self = this
+    if (!self.isInstance()) return false
+    CServer.network.destroy(self.name)
+    return true
+}
+
+on(exec) {
+    const self = this
+    if (!self.isInstance() || !CUtility.isFunction(exec)) return false
+    const vid = CUtility.fetchVID(exec)
+    if (!self.isCallback) {
+        if (self.handler[vid]) return false
+        self.handler[vid] = {
+            exec: exec
+        }
+    }
+    else {
+        if (self.handler) return false
+        self.handler = {
+            exec: exec
+        }
+    }
+    return true
+}
+
+off(exec) {
+    const self = this
+    if (!self.isInstance() || !CUtility.isFunction(exec)) return false
+    if (!self.isCallback) {
+        const vid = CUtility.fetchVID(exec)
+        if (!self.handler[vid]) return false
+        delete self.handler[vid]
+    }
+    else {
+        if (!self.handler || (CUtility.fetchVID(exec) != CUtility.fetchVID(self.handler))) return false
+        self.handler = false
+    }
+    return true
+}
+
+emit(...cArgs) {
+    const self = this
+    if (!self.isInstance() || self.isCallback) return false
+    for (const i in self.handler) {
+        const j = self.handler[i]
+        j.exec(...cArgs)
+    }
+    return true
+}
+
+async emitCallback(...cArgs) {
+    const self = this
+    if (!self.isInstance() || !self.isCallback || !self.handler) return false
+    return await self.handler.exec(...cArgs)
+}
