692882dce349b90907ffc106f2ee790f12ee49b6
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-25 16:43:29 +0300

    Init room class

--- src/managers/room.js
+++ src/managers/room.js
@@ -17,54 +17,55 @@ const CUtility = require("../utilities")
 const CServer = require("./server")
 
 
-///////////////////////
-// Instance Members //
-///////////////////////
+//////////////////
+// Class: Room //
+//////////////////
 
-CServer.socket.addInstanceMethod("isRoom", function(self, name) {
+CServer.room = CUtility.createClass({
+    buffer: {}
+})
+
+
+/////////////////////
+// Static Members //
+/////////////////////
+
+CServer.room.addMethod("isVoid", function(name) {
+    return (CUtility.isString(name) && !CUtility.isObject(CServer.room.buffer[name]) && true) || false
+})
+
+CServer.room.addMethod("fetch", function(name) {
+    return (!CServer.room.isVoid(name) && CServer.room.buffer[name]) || false
+})
+
+CServer.room.addMethod("create", function(name, ...cArgs) {
+    if (!CServer.isConnected(true) || !CServer.room.isVoid(name)) return false
+    CServer.room.buffer[name] = new CServer.room(name, ...cArgs)
+    return CServer.room.buffer[name]
+})
+
+CServer.room.addMethod("destroy", function(name) {
+    if (CServer.room.isVoid(name)) return false
+    CServer.room.buffer[name].isUnloaded = true
+    delete CServer.room.buffer[name]
+    return true
+})
+
+
+///////////////////////
+// Instance Mmebers //
+//////////////////////
+
+CServer.room.addMethod("constructor", function(self, name, isCallback) {
+    self.name = name
+})
+
+CServer.room.addInstanceMethod("isInstance", function(self) {
+    return (!self.isUnloaded && !CServer.room.isVoid(self.name) && true) || false
+})
+
+CServer.room.addInstanceMethod("destroy", function(self) {
     if (!self.isInstance()) return false
-    return (CUtility.isString(name) && CUtility.isObject(self.room[name]) && true) || false
+    CServer.room.destroy(self.name)
+    return true
 })
-
-CServer.socket.addInstanceMethod("isInRoom", function(self, name, client) {
-    if (!self.isInstance() || !self.isRoom(name)) return false
-    if (CUtility.isServer && !self.isClient(client)) return false
-    const vid = (CUtility.isServer && CUtility.fetchVID(client)) || CUtility.fetchVID(self)
-    return (CUtility.isObject(self.room[name]) && self.room[name][vid] && true) || false
-})
-
-if (!CUtility.isServer) {
-
-}
-else {
-    ///////////////////////
-    // Instance Members //
-    ///////////////////////
-
-    CServer.socket.addInstanceMethod("createRoom", function(self, name) {
-        if (!self.isInstance() || self.isRoom(name)) return false
-        self.room[name] = {}
-        return true
-    })
-
-    CServer.socket.addInstanceMethod("destroyRoom", function(self, name) {
-        if (!self.isInstance() || !self.isRoom(name)) return false
-        delete self.room[name]
-        return true
-    })
-
-    CServer.socket.addInstanceMethod("joinRoom", function(self, name, client) {
-        if (!self.isInstance() || !self.isClient(client) || !self.isRoom(name) || self.isInRoom(name, client)) return false
-        const vid = CUtility.fetchVID(client)
-        self.room[name][vid] = true
-        return true
-    })
-
-    CServer.socket.addInstanceMethod("emitRoom", function(self, name, network, ...cArgs) {
-        if (!self.isInstance() || !self.isRoom(name)) return false
-        for (const i in self.room[name]) {
-            self.emit(network, self.instance[i], ...cArgs)
-        }
-        return true
-    })
-}
