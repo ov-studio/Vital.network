de733dc07e5c21758efce162988546cb7d9c5a7f
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-27 17:10:05 +0300

    Adding support for client  side `socket.onClientConnect` & `socket.onClientDisconnect`

--- src/managers/socket/index.js
+++ src/managers/socket/index.js
@@ -102,10 +102,16 @@ if (!CUtility.isServer) {
             var cResolver = false
             self.config.isAwaiting = new Promise((resolver) => cResolver = resolver)
             self.server = new WebSocket(`${((CServer.config.protocol == "https") && "wss") || "ws"}://${CServer.config.hostname}:${CServer.config.port}/${self.route}`)
+            const vid = CUtility.fetchVID(self.server)
             self.server.onopen = function() {
                 self.config.isAwaiting = null
                 self.config.isConnected = true
                 cResolver(self.config.isConnected)
+                if (CUtility.isFunction(self.onClientConnect)) self.onClientConnect(self.server, vid)
+                return true
+            }
+            self.server.onclose = function() {
+                if (CUtility.isFunction(self.onClientDisconnect)) self.onClientDisconnect(self.server, vid)
                 return true
             }
             self.server.onerror = function(error) {
