cc20f5fc8b8525bac4f70ac2b2db7f3563fe510c
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-29 21:33:35 +0300

    Added desc comments to room manager

--- src/managers/room.js
+++ src/managers/room.js
@@ -29,20 +29,24 @@ CServer.room = CUtility.createClass({
 // Static Members //
 /////////////////////
 
+// @Desc: Verifies whether the room is void
 CServer.room.addMethod("isVoid", function(name) {
     return (CUtility.isString(name) && !CUtility.isObject(CServer.room.buffer[name]) && true) || false
 })
 
+// @Desc: Fetches room instance by name
 CServer.room.addMethod("fetch", function(name) {
     return (!CServer.room.isVoid(name) && CServer.room.buffer[name]) || false
 })
 
+// @Desc: Creates a fresh room w/ specified name
 CServer.room.addMethod("create", function(name, ...cArgs) {
     if (!CServer.isConnected(true) || !CServer.room.isVoid(name)) return false
     CServer.room.buffer[name] = new CServer.room(name, ...cArgs)
     return CServer.room.buffer[name]
 })
 
+// @Desc: Destroys an existing room by specified name
 CServer.room.addMethod("destroy", function(name) {
     if (CServer.room.isVoid(name)) return false
     CServer.room.buffer[name].isUnloaded = true
@@ -55,14 +59,17 @@ CServer.room.addMethod("destroy", function(name) {
 // Instance Mmebers //
 //////////////////////
 
+// @Desc: Instance Constructor
 CServer.room.addMethod("constructor", function(self, name) {
     self.name = name
 }, "isInstance")
 
+// @Desc: Verifies instance's validity
 CServer.room.addInstanceMethod("isInstance", function(self) {
     return (!self.isUnloaded && !CServer.room.isVoid(self.name) && true) || false
 })
 
+// @Desc: Destroys the instance
 CServer.room.addInstanceMethod("destroy", function(self) {
     CServer.room.destroy(self.name)
     return true
