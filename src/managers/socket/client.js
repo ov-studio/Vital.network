c5c21337a7f1d731119d33e88b8de2fdc8ddf121
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-29 21:48:24 +0300

    Update client.js

--- src/managers/socket/client.js
+++ src/managers/socket/client.js
@@ -29,11 +29,13 @@ CServer.socket.client = CUtility.createClass({
 // Static Members //
 /////////////////////
 
+// @Desc: Fetches client instance by VID or socket
 CServer.socket.client.addMethod("fetch", function(vid, socket, fetchSocket) {
     vid = vid || CUtility.fetchVID(socket, null, true)
     return (vid && CUtility.isObject(CServer.socket.client.buffer[vid]) && CServer.socket.client.buffer[vid] && ((fetchSocket && CServer.socket.client.buffer[vid].socket) || CServer.socket.client.buffer[vid])) || false
 })
 
+// @Desc: Creates a fresh client w/ specified socket
 CServer.socket.client.addMethod("create", function(socket) {
     if (!CUtility.isObject(socket) || CServer.socket.client.fetch(null, socket)) return false
     const cInstance = new CServer.socket.client(socket)
@@ -42,6 +44,7 @@ CServer.socket.client.addMethod("create", function(socket) {
     return cInstance
 })
 
+// @Desc: Destroys an existing client by specified VID or socket
 CServer.socket.client.addMethod("destroy", function(vid, socket) {
     vid = vid || CUtility.fetchVID(socket, null, true)
     if (!CServer.socket.client.fetch(vid)) return false
@@ -55,15 +58,18 @@ CServer.socket.client.addMethod("destroy", function(vid, socket) {
 // Instance Members //
 ///////////////////////
 
+// @Desc: Instance Constructor
 CServer.socket.client.addMethod("constructor", function(self, socket) {
     CUtility.fetchVID(self, CUtility.fetchVID(socket))
     self.socket = socket
 }, "isInstance")
 
+// @Desc: Verifies instance's validity
 CServer.socket.client.addInstanceMethod("isInstance", function(self) {
     return (!self.isUnloaded && CServer.socket.client.fetch(null, self.socket) && true) || false
 })
 
+// @Desc: Destroys the instance
 CServer.socket.client.addInstanceMethod("destroy", function(self) {
     CServer.socket.client.destroy(null, self.socket)
     return true
