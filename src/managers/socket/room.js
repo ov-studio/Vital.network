7296f56b3487e0df647f1501a48670758d313aff
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-30 15:40:36 +0300

    Added server side `socket.onClientJoinRoom` & `socket.onClientLeaveRoom`

--- src/managers/socket/room.js
+++ src/managers/socket/room.js
@@ -86,6 +86,7 @@ else {
         self.room[name].member[client] = true
         const clientInstance = CServer.socket.client.fetch(client)
         clientInstance.socket.send(JSON.stringify({room: name, action: "join"}))
+        if (CUtility.isFunction(self.onClientJoinRoom)) self.onClientJoinRoom(name, client)
         return true
     })
 
@@ -95,6 +96,7 @@ else {
         delete self.room[name].member[client]
         const clientInstance = CServer.socket.client.fetch(client)
         clientInstance.socket.send(JSON.stringify({room: name, action: "leave"}))
+        if (CUtility.isFunction(self.onClientLeaveRoom)) self.onClientLeaveRoom(name, client)
         return true
     })
 
