6e2d5b18c640c9dc7cec8bdb07d5c280f7af8d44
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-05 01:33:34 +0300

    Update index.js

--- src/managers/socket/index.js
+++ src/managers/socket/index.js
@@ -222,7 +222,7 @@ CNetwork.fetch("vNetworkify:Server:onConnect").on(function(server) {
                     private.onHeartbeat(clientInstance)
                     CUtility.exec(self.onClientConnect, client)
                     clientInstance.socket.onclose = function() {
-                        const reason = (private.client[client]["@disconnect"] && private.client[client]["@disconnect"].reason) || private.reason || "client-disconnected"
+                        const reason = (clientInstance["@disconnect"] && clientInstance["@disconnect"].reason) || (private["@disconnect"] && private["@disconnect"].reason) || "client-disconnected"
                         for (const i in clientInstance.socket.room) {
                             self.leaveRoom(i, client)
                         }
