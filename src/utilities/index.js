ed8a85192eff90ae0ff1ddad3c6260954835b64e
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-04 09:50:50 +0300

    Update index.js

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -26,6 +26,7 @@ CUtility.global = (CUtility.isServer && global) || window
 CUtility.toBase64 = (!CUtility.isServer && btoa.bind(window)) || function(data) { return Buffer.from(data).toString("base64") }
 CUtility.fromBase64 = (!CUtility.isServer && atob.bind(window)) || function(data) { return Buffer.from(data, "base64").toString("binary") }
 CUtility.identifier = CUtility.toBase64(`vNetworkify-${(CUtility.isServer && "Server") || "Client"}`)
+CUtility.version = process.env.npm_package_version
 
 // @Desc: Executes the specified handler
 CUtility.exec = function(exec, ...cArgs) {
