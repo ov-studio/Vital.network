61299f9df080f59a9cab5b33f9250c33bde0333e
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-04 11:40:33 +0300

    Update index.js

--- src/managers/socket/index.js
+++ src/managers/socket/index.js
@@ -14,7 +14,6 @@
 
 const CWS = require("ws")
 const CUtility = require("../../utilities")
-const CServer = require("../server")
 const {onSocketInitialize, onSocketMessage} = require("./parser")
 
 
