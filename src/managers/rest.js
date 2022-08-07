fc2685e6559da1407820bd69a1aa3b99dc6322a6
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-07 15:53:59 +0300

    Update rest.js

--- src/managers/rest.js
+++ src/managers/rest.js
@@ -20,12 +20,12 @@ const CNetwork = require("../utilities/network")
 // Class: Rest //
 //////////////////
 
-CNetwork.fetch("vNetworkify:Server:onConnect").on(function(server) {
+CNetwork.fetch("vNetworkify:Server:onConnect").on((server) => {
     const CRest = CUtility.createClass()
     server.public.rest = CRest.public
     CRest.private.post = {}, CRest.private.get = {}, CRest.private.put = {}, CRest.private.delete = {}
 
-    CNetwork.fetch("vNetworkify:Server:onDisconnect").on(function(__server) {
+    CNetwork.fetch("vNetworkify:Server:onDisconnect").on((__server) => {
         if ((server.public != __server.public) || (server.private != __server.private)) return false
         CRest.private.isUnloaded = true
         delete server.public.rest
@@ -37,7 +37,7 @@ CNetwork.fetch("vNetworkify:Server:onConnect").on(function(server) {
     /////////////////////
 
     // @Desc: Requests a fetch on specified REST API 
-    CRest.public.addMethod("fetch", function(type, ...cArgs) {
+    CRest.public.addMethod("fetch", (type, ...cArgs) => {
         if (CRest.private.isUnloaded) return false
         if (!CUtility.isObject(CRest.private[type])) return false
         return server.private.instance.http[type](...cArgs)
@@ -45,27 +45,27 @@ CNetwork.fetch("vNetworkify:Server:onConnect").on(function(server) {
 
     if (CUtility.isServer) {
         // @Desc: Verifies whether the REST API is void
-        CRest.public.addMethod("isVoid", function(type, route) {
+        CRest.public.addMethod("isVoid", (type, route) => {
             if (CRest.private.isUnloaded) return false
             return (CUtility.isString(type) && CUtility.isString(route) && CUtility.isObject(CRest.private[type]) && (!CRest.private[type][route] || !CRest.private[type][route].handler) && true) || false
         })
         
         // @Desc: Creates a fresh REST API
-        CRest.public.addMethod("create", function(type, route, exec) {
+        CRest.public.addMethod("create", (type, route, exec) => {
             if (CRest.private.isUnloaded) return false
             if (!CRest.public.isVoid(type, route) || !CUtility.isFunction(exec)) return false
             CRest.private[type][route] = CRest.private[type][route] || {}
-            CRest.private[type][route].manager = CRest.private[type][route].manager || function(...cArgs) {
+            CRest.private[type][route].manager = CRest.private[type][route].manager || ((...cArgs) => {
                 CUtility.exec(CRest.private[type][route].handler, ...cArgs)
                 return true
-            }
+            })
             CRest.private[type][route].handler = exec
             server.private.instance.express[type](`/${route}`, CRest.private[type][route].manager)
             return true
         })
         
         // @Desc: Destroys an existing REST API
-        CRest.public.addMethod("destroy", function(type, route) {
+        CRest.public.addMethod("destroy", (type, route) => {
             if (CRest.private.isUnloaded) return false
             if ((route == server.private.healthpoint) || CRest.public.isVoid(type, route)) return false
             delete CRest.private[type][route].handler
@@ -73,7 +73,7 @@ CNetwork.fetch("vNetworkify:Server:onConnect").on(function(server) {
         })
         
         // @Desc: Routing middleware
-        CRest.public.addMethod("onMiddleware", function(request, response, next) {
+        CRest.public.addMethod("onMiddleware", (request, response, next) => {
             if (CRest.private.isUnloaded) return false
             const type = request.method.toLowerCase()
             const route = request.url.slice(1)
