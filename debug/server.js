e775d9dd7c7d003b1f84912f6c41b73d84892c29
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-06 17:47:33 +0300

    Update server.js

--- debug/server.js
+++ debug/server.js
@@ -113,7 +113,7 @@ async function debug() {
     cServer.rest.create("get", "", function(request, response) {
         response.status(200).send({test: "Updated Status Message"})
     })
-    var restAPIResult = await cServer.rest.fetch("get", "https://raw.githubusercontent.com/ov-studio/vNetwork/main/package.json")
+    var restAPIResult = await cServer.rest.fetch("get", "https://raw.githubusercontent.com/ov-studio/vNetworkify/main/package.json")
     vKit.print(JSON.parse(restAPIResult))
 }
 debug()
